dd4d7f103b918ba6841c263ac33c8b84203dba96
Arun-Niranjan <arun.niranjan.12@ucl.ac.uk>
2024-12-23 16:40:06 +0000

    Fix environment variables and test basic open ai embeddings

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import getpass
+import os
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large")
+
+
 def main():
     print("Hello from prouting!")
+
+    embeddings = embeddings_model.embed_documents(
+        [
+            "Hi there!",
+            "Oh, hello!",
+            "What's your name?",
+            "My friends call me World",
+            "Hello World!",
+        ]
+    )
+    print(len(embeddings), len(embeddings[0]))
 
 
 if __name__ == "__main__":
